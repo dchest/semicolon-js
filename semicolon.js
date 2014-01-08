4b89eb24e607cb4493b7b962f33b786a3e7f0532
Rich Wareham <rjw57@cam.ac.uk>
2014-01-08 13:35:33 +0000

    add licence banner
    To be enterprise-ready, all source files must include a licensing banner
    to allow for IP audits.

--- semicolon.js
+++ semicolon.js
@@ -1,1 +1,9 @@
+/* Written in 2014 by Dmitry Chestnykh
+ *
+ * https://github.com/dchest/semicolon-js
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty.
+ */
 ;
